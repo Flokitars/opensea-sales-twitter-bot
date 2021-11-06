dcc90181f97178daad7cf44f96d0992d56cd7dbf
Flokitars <93797171+Flokitars@users.noreply.github.com>
2021-11-05 22:59:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,70 +3,42 @@ const _ = require('lodash');
 const moment = require('moment');
 const { ethers } = require('ethers');
 const tweet = require('./tweet');
-const cache = require('./cache');
 
-// Format tweet text
 function formatAndSendTweet(event) {
-    // Handle both individual items + bundle sales
-    const assetName = _.get(event, ['asset', 'name'], _.get(event, ['asset_bundle', 'name']));
-    const openseaLink = _.get(event, ['asset', 'permalink'], _.get(event, ['asset_bundle', 'permalink']));
-
+    const tokenName = _.get(event, ['asset', 'name']);
+    const image = _.get(event, ['asset', 'image_url']);
+    const openseaLink = _.get(event, ['asset', 'permalink']);
     const totalPrice = _.get(event, 'total_price');
+    const usdValue = _.get(event, ['payment_token', 'usd_price']);
 
-    const tokenDecimals = _.get(event, ['payment_token', 'decimals']);
-    const tokenUsdPrice = _.get(event, ['payment_token', 'usd_price']);
-    const tokenEthPrice = _.get(event, ['payment_token', 'eth_price']);
+    const formattedEthPrice = ethers.utils.formatEther(totalPrice.toString());
+    const formattedUsdPrice = (formattedEthPrice * usdValue).toFixed(2);
 
-    const formattedUnits = ethers.utils.formatUnits(totalPrice, tokenDecimals);
-    const formattedEthPrice = formattedUnits * tokenEthPrice;
-    const formattedUsdPrice = formattedUnits * tokenUsdPrice;
-
-    const tweetText = `${assetName} bought for ${formattedEthPrice}${ethers.constants.EtherSymbol} ($${Number(formattedUsdPrice).toFixed(2)}) #NFT ${openseaLink}`;
+    const tweetText = `${tokenName} was adopted for Ξ${formattedEthPrice} ($${formattedUsdPrice})\n ${openseaLink}\n #NFT #NFTs #CryptoPunks #PunkBabies #NFTCommunity #altpunks`;
 
     console.log(tweetText);
 
-    // OPTIONAL PREFERENCE - don't tweet out sales below X ETH (default is 1 ETH - change to what you prefer)
-    // if (Number(formattedEthPrice) < 1) {
-    //     console.log(`${assetName} sold below tweet price (${formattedEthPrice} ETH).`);
-    //     return;
-    // }
-
-    // OPTIONAL PREFERENCE - if you want the tweet to include an attached image instead of just text
-    // const imageUrl = _.get(event, ['asset', 'image_url']);
-    // return tweet.tweetWithImage(tweetText, imageUrl);
-
-    return tweet.tweet(tweetText);
+    return tweet.handleDupesAndTweet(tokenName, tweetText, image);
 }
 
-// Poll OpenSea every 60 seconds & retrieve all sales for a given collection in either the time since the last sale OR in the last minute
+// Poll OpenSea every minute & retrieve all sales for a given collection in the last minute
+// Then pass those events over to the formatter before tweeting
 setInterval(() => {
-    const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('minute').subtract(59, "seconds").unix();
-
-    console.log(`Last sale (in seconds since Unix epoch): ${cache.get('lastSaleTime', null)}`);
+    const lastMinute = moment().startOf('minute').subtract(59, "seconds").unix() * 1000;
 
     axios.get('https://api.opensea.io/api/v1/events', {
         params: {
             collection_slug: process.env.OPENSEA_COLLECTION_SLUG,
             event_type: 'successful',
-            occurred_after: lastSaleTime,
+            occurred_after: lastMinute,
             only_opensea: 'false'
         }
     }).then((response) => {
         const events = _.get(response, ['data', 'asset_events']);
 
-        const sortedEvents = _.sortBy(events, function(event) {
-            const created = _.get(event, 'created_date');
+        console.log(`${events.length} sales in the last minute...`);
 
-            return new Date(created);
-        })
-
-        console.log(`${events.length} sales since the last one...`);
-
-        _.each(sortedEvents, (event) => {
-            const created = _.get(event, 'created_date');
-
-            cache.set('lastSaleTime', moment(created).unix());
-
+        _.each(events, (event) => {
             return formatAndSendTweet(event);
         });
     }).catch((error) => {
