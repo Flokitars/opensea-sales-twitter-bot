fa872a8c52d0ef5be727a8961f0860b0c9a130d3
Flokitars <93797171+Flokitars@users.noreply.github.com>
2021-11-05 23:13:12 -0400

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 const twit = require('twit');
+const moment = require('moment');
+const _ = require('lodash');
 
 const twitterConfig = {
     consumer_key: process.env.CONSUMER_KEY,
@@ -10,23 +12,40 @@ const twitterConfig = {
 
 const twitterClient = new twit(twitterConfig);
 
-// Tweet a text-based status
-async function tweet(tweetText) {
-    const tweet = {
-        status: tweetText,
-    };
-
-    twitterClient.post('statuses/update', tweet, (error, tweet, response) => {
+// OpenSea doesn't give us access to Webhooks; need to poll every 60 seconds
+// Occasionaly in the split second of delay, dupelicates are retrieved - filter them out here
+async function handleDupesAndTweet(tokenName, tweetText, imageUrl) {
+    // Search our twitter account's recent tweets for anything exactly matching our new tweet's text
+    twitterClient.get('search/tweets', { q: tokenName, count: 1, result_type: 'recent' }, (error, data, response) => {
         if (!error) {
-            console.log(`Successfully tweeted: ${tweetText}`);
+            const statuses = _.get(data, 'statuses');
+
+            // No duplicate statuses found
+            if (_.isEmpty(data) || _.isEmpty(statuses)) {
+                console.log('No duplicate statuses found, continuing to tweet...');
+
+                return tweet(tweetText, imageUrl);
+            }
+
+            const mostRecentMatchingTweetCreatedAt = _.get(statuses[0], 'created_at');
+            const statusOlderThan10Mins = moment(mostRecentMatchingTweetCreatedAt).isBefore(moment().subtract(10, 'minutes'));
+
+            // Status found is older than 10 minutes, not a cached transaction, just sold at same price
+            if (statusOlderThan10Mins) {
+                console.log('Previous status is older than 10 minutes, continuing to tweet...');
+
+                return tweet(tweetText, imageUrl);
+            }
+
+            console.error('Tweet is a duplicate; possible delayed transaction retrieved from OpenSea');
         } else {
-            console.error(error);
+            console.error(err);
         }
     });
 }
 
-// OPTIONAL - use this method if you want the tweet to include the full image file of the OpenSea item in the tweet.
-async function tweetWithImage(tweetText, imageUrl) {
+// Upload image of item retrieved from OpenSea & then tweet that image + provided text
+async function tweet(tweetText, imageUrl) {
     // Format our image to base64
     const processedImage = await getBase64(imageUrl);
 
@@ -57,6 +76,5 @@ function getBase64(url) {
 }
 
 module.exports = {
-    tweet: tweet,
-    tweetWithImage: tweetWithImage
+    handleDupesAndTweet: handleDupesAndTweet
 };
